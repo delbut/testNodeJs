86fa050b5fdad8ccb8d2b0db995e00b4922b42b1
Delbut <delbut.maxime@gmail.com>
2016-06-03 14:57:44 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,14 @@
+/**
+ * Created by maxime on 03/06/16.
+ */
+var express = require('express');
+var api = express();
+
+api.get('/', function (req, res, next) {
+    res.send('Hello world')
+});
+
+console.log("Start on port 3000")
+api.listen(3000);
+
+module.exports = api;
---
+++ test/api.spec.js
@@ -0,0 +1,50 @@
+/**
+ * Created by maxime on 03/06/16.
+ */
+
+var request = require('supertest');
+var api = require ('../index')
+
+describe('API', function () {
+    describe('Contacts /', function () {
+        it('GET /contacts should return list of registered contacts', function () {
+            return request(api)
+                .get('/contacts')
+                .send()
+                .expect(200);
+        });
+        it('GET /contacts/:name should return contact of registered by name', function () {
+            return request(api)
+                .get('/contacts/foo')
+                .send()
+                .expect(200)
+                .expect(function (res, err) {
+                    return res.body instanceof Array
+                });
+        });
+        it('GET /contacts/:name should return 404 response', function () {
+            return request(api)
+                .get('/contacts/none')
+                .send()
+                .expect(404);
+        });
+        it('POST /contacts should create contact', function () {
+            return request(api)
+                .post('/contacts')
+                .send({name: "edward"})
+                .expect(200);
+        });
+        it('PUT /contacts/:name/:new update all contact with the same name', function () {
+            return request(api)
+                .put('/contacts/foo/bar')
+                .send()
+                .expect(200);
+        });
+        it('DELETE /contacts/:name delete all contact with the same name', function () {
+            return request(api)
+                .delete('/contacts/foo')
+                .send()
+                .expect(200);
+        })
+    });
+});
